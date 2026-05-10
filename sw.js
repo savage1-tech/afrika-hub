dd6ef674aca800c298e6382b699f84ee93fe96b1
savage1-tech <akisowonvictor11@gmail.com>
2026-05-11 00:08:58 +0100

    Add service worker for offline PWA support

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install', (e) => { e.waitUntil(caches.open('afrika-hub-cache').then((cache) => cache.addAll(['/','index.html'])); }); self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request))); });
